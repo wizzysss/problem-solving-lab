b53fcef9d974e35678bede6e3729d502732074fe
wizzysss <aravindhjasu25@gmail.com>
2026-01-16 21:08:08 +0530

    Second Largest second way

--- Random DSA/Simple problems/SecondLaregestinArray/SecondLargest.java
+++ Random DSA/Simple problems/SecondLaregestinArray/SecondLargest.java
@@ -2,15 +2,24 @@
 
 // Task:
 // Given an array of integers, find the second largest number.
+import java.util.Arrays;
 
 
 public class SecondLargest{
 
 
+    public int sortarraymethod(int[] arr){
+
+        int[] sortedarr =arr.clone();
+        Arrays.sort(sortedarr);
+        int seclar =sortedarr[sortedarr.length-2];
+        return seclar;
+    }
+
     public static void main(String[] args) {
         // Scanner sc = new Scanner(System.in);
 
-        int[] number ={1,1,1,1};
+        int[] number ={1,5,3,1};
         int largest =Integer.MIN_VALUE;
         int secondLargest =Integer.MIN_VALUE;
 
@@ -24,12 +33,20 @@ public class SecondLargest{
             }
         }
 
-        if (secondLargest==Integer.MIN_VALUE) {
-            System.out.println("Ther is no second largest");
-        }
-        else{
-            System.out.println("Second largest is "+secondLargest);
-        }
+        //straight away way
+
+        // if (secondLargest==Integer.MIN_VALUE) {
+        //     System.out.println("Ther is no second largest");
+        // }
+        // else{
+        //     System.out.println("Second largest is "+secondLargest);
+        // }
+
+        //method with sorted way
+        SecondLargest sl = new SecondLargest();
+        secondLargest = sl.sortarraymethod(number);
+
+        System.out.println("Second largest is "+secondLargest);
 
     }
     
